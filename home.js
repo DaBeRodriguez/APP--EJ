719be4f682aa82ad0422695e4273081978e90cd1
jarumi <jarumi.flores15@gmail.com>
2023-03-17 11:32:25 -0500

    andrea

--- home.js
+++ home.js
@@ -1,8 +1,1 @@
-function publicar(){
-var mensaje = document.getElementById("mensaje")
-var publi = document.getElementById(mensaje)
-document.getElementById("publi").value
-console.log("publicar")
-}
-
 
