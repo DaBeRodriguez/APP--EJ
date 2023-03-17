8a9c4631ee29d3ad65d7f9dd055c309780989872
jarumi <jarumi.flores15@gmail.com>
2023-03-17 09:54:22 -0500

    andre

---
+++ home.js
@@ -0,0 +1,8 @@
+function publicar(){
+var mensaje = document.getElementById("mensaje")
+var publi = document.getElementById(mensaje)
+document.getElementById("publi").value
+console.log("publicar")
+}
+
+
