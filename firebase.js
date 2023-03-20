8ccc07af7263f8e434ac595c35ba2d5275034ff4
Noelia <noeliachaconpimentel@gmail.com>
2023-03-20 11:37:57 -0500

    ..

--- firebase.js
+++ firebase.js
@@ -10,8 +10,9 @@ $(document).ready(function () {
   };
 
   // No olvidar poner el => Firebase. (antes de "initializeApp")
-  const app = firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
   console.log(app);
+const db =  firebase.firestore();
 
 
 })
@@ -19,6 +20,8 @@ $(document).ready(function () {
 // Registrar usuarios
 $("#registrate").click(function () {
   // capturar email y contraseña
+
+  let nombre = $("#Nombre").val();
 
   let email = $("#Email1").val();
   let password = $("#Password1").val();
@@ -31,12 +34,12 @@ $("#registrate").click(function () {
       // Signed in
       var user = userCredential.user;
 
-
       Swal.fire(
         'Listo!',
         'Su cuenta a sido creada con éxito',
         // '<a href="index.html">Ir al inicio</a>'      En este punto debo de poner la redireccion
       )
+      addNombre(nombre);
 
     })
     .catch((error) => {
@@ -54,20 +57,22 @@ $("#registrate").click(function () {
 // Ingresar con cuenta registrada
 
 $("#ingresar").click(function () {
-  let email = $("#Email").val();
-  let password = $("#Password").val();
+  let email = $("#Email1").val();
+  let password = $("#Password1").val();
 
   firebase.auth().signInWithEmailAndPassword(email, password)
     .then((userCredential) => {
       // Signed in
       var user = userCredential.user;
       // ...
-      location.href = "pagina.html";
+      location.href = "home.html";
+      // alert("hola")
 
     })
     .catch((error) => {
       var errorCode = error.code;
       var errorMessage = error.message;
+      console.log(errorCode,errorMessage)
     });
 })
 
@@ -125,12 +130,95 @@ $("#salir").click(function () {
 });
 
 
+// Actualiza el perfil de un usuario
 
-// $("#publicar").click(
-//   function () {
+function addNombre(nombre) {
 
-//   }
-// )
+
+  const user = firebase.auth().currentUser;
+
+  user.updateProfile({
+    displayName: "Jane Q. User",
+    // photoURL: "https://example.com/jane-q-user/profile.jpg"
+
+  }).then(() => {
+    // Update successful
+
+    // ...
+  }).catch((error) => {
+    // An error occurred
+    // ...
+  });
+
+};
+
+
+// Mostrar usuarios activos
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    var uid = user.uid;
+    var email = user.email;
+    var usuario = user.displayName;
+
+    console.log(uid,email.usuario);
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
+
+// CREAR PUBLICAIONES
+$("#publicar").click(function (e) {
+ e.preventDefault();
+let post = $("#texto");val();
+
+const user = firebase.auth().currentUser;
+db.collection("posteos").add({
+ _publicacion:post,
+_idUser:user.iud,
+ _nombreUser:user.displayName, 
+})
+.then((docRef) => {
+  console.log("Document written with ID: ", docRef.id);
+})
+.catch((error) => {
+  console.error("Error adding document: ", error);
+});
+})
+
+
+
+
+
+// Actualizar nombre por opciones
+
+// $("#actualizar_nombre").click(function () {
+//   alert("hola");
+// let usuario = $("#Nombre").val();
+//   const user = firebase.auth().currentUser;
+
+  // user.updateProfile({ 
+  //   displayName: usuario,
+  //   // photoURL: "https://example.com/jane-q-user/profile.jpg"
+
+  // }).then(() => {
+  //   // Update successful
+  // alert("hola");
+  //   // ...
+  // }).catch((error) => {
+  //   // An error occurred
+  //   // ...
+  // });
+
+
+
+
+
+
 
 
 
